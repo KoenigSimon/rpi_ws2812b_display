6804b78db8b2183e0b33972c8e6326f5c97e6a07
KoenigSimon <koenig.simon@t-online.de>
2022-07-12 16:57:44 +0100

    add install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+
+sudo apt-get install xvfb netpbm x11-apps -y
+sudo apt-get install python3-pip -y
+sudo apt-get install freeglut3-dev -y
+
+pip3 install rpi_ws281x pyopengl pyopengl_accelerate
